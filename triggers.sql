c8832660072060eb8ff740069c6815345dbe5791
yusnof <yusnofal@gmail.com>
2025-02-24 22:08:45 +0100

    added some comments

--- triggers.sql
+++ triggers.sql
@@ -1,26 +1,24 @@
 CREATE FUNCTION CountRegistered() RETURNS trigger AS $$
-    --Check if student is allowed to join course(pre)
-    --Check if LC
-        --count capacity
-        --Add student to waitingList or Registered
-        --helper function to WaitingList insert and update positions.
-        --
     DECLARE
     totalReg INT; 
     capacity INT; 
     waiting INT;
     BEGIN 
+    --We first cheack if the student aldready taken the course
     IF EXISTS (SELECT course FROM PassedCourses WHERE NEW.student = student AND NEW.course = course) THEN 
     RAISE EXCEPTION 'Student has passed course';
     ELSE 
+    --We cheack if the student meet the prerequisites of this specific course. 
     IF NOT EXISTS (((SELECT prerequisite FROM PreRequisites WHERE NEW.course = course))
         EXCEPT
         (SELECT course FROM PassedCourses WHERE NEW.student = student )) 
     THEN 
+        -- The cheack if the course is limited or not. 
         IF EXISTS (SELECT code FROM LimitedCourses WHERE new.course = code) THEN 
         totalReg := (SELECT COUNT(course) FROM Registrations WHERE course=NEW.course AND Registrations.status = 'registered');
         capacity := (SELECT LimitedCourses.capacity FROM LimitedCourses WHERE code=NEW.course);
         waiting := (SELECT COUNT(Registrations.status) FROM Registrations where course = NEW.course AND Registrations.status='waiting');
+        --if the calculation results that is no space we add to WaitingList
          IF (totalReg >= capacity) THEN
             INSERT INTO WaitingList Values (NEW.student, NEW.course, (waiting+1));
         --RAISE EXCEPTION 'shit is illegal/You cannot join the course, you are too late';
@@ -45,8 +43,8 @@ next_student TEXT;
 next_course TEXT;
 lc_cap INT;
 totalReg INT;
-
 BEGIN
+-- we check if the student is registered in the course
 IF EXISTS (SELECT student FROM Registrations WHERE OLD.student = student AND OLD.course = course)
 THEN 
     DELETE FROM Registered WHERE OLD.student = student AND OLD.course = course;
@@ -55,6 +53,7 @@ THEN
     SELECT capacity INTO lc_cap FROM LimitedCourses WHERE OLD.course = code;
     SELECT COUNT(course) INTO totalReg FROM Registered WHERE OLD.course = course;
 
+    -- if this student is on the waitingList and there is space in the course 
     IF (next_student IS NOT NULL AND lc_cap > totalReg) 
     THEN
         INSERT INTO Registered VALUES (next_student, next_course);
@@ -62,6 +61,9 @@ THEN
         UPDATE WaitingList SET position = position - 1 WHERE OLD.course = course;
     END IF;
 END IF;
+-- Here we check if the student is on the waitingList and if its true, we remove from 
+-- the waitingList and the shift the position of the remaining students to insure the 
+--correct position 
 IF EXISTS (SELECT 1 FROM WaitingList WHERE OLD.student = student AND OLD.course = course)
 THEN
     SELECT position INTO removed_position FROM WaitingList WHERE OLD.student = student AND OLD.course = course;
