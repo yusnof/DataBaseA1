faa8720652dbf8007e281975272734df2f3b29c2
GunnarJohansson <146834192+GunnarJohansson@users.noreply.github.com>
2025-02-24 13:03:28 +0100

    Hello Yousef:
    I updated triggers to use Registrations. It passes some test but not all.
    
    We do need to run more tests.
    
    Please read,
    
    Best Regards,
    India

--- triggers.sql
+++ triggers.sql
@@ -8,30 +8,33 @@ CREATE FUNCTION CountRegistered() RETURNS trigger AS $$
     DECLARE
     totalReg INT; 
     capacity INT; 
+    waiting INT;
     BEGIN    
     IF NOT EXISTS ((SELECT course FROM PreRequisites WHERE NEW.course = course)
         EXCEPT
         (SELECT course FROM Taken WHERE NEW.student = student))
     THEN 
         IF EXISTS (SELECT code FROM LimitedCourses WHERE new.course = code) THEN 
-        totalReg := (SELECT COUNT(course) FROM Registered WHERE course=NEW.course);
-        capacity := (SELECT capacity FROM LimitedCourses WHERE course=NEW.course);
+        totalReg := (SELECT COUNT(course) FROM Registrations WHERE course=NEW.course);
+        capacity := (SELECT LimitedCourses.capacity FROM LimitedCourses WHERE code=NEW.course);
+        waiting := (SELECT COUNT(Registrations.status) FROM Registrations where Registrations.status='waiting');
          IF (totalReg >= capacity) THEN
-            INSERT INTO WaitingList Values (NEW.student, New.code);
+            INSERT INTO WaitingList Values (NEW.student, NEW.course, (waiting+1));
         --RAISE EXCEPTION 'shit is illegal/You cannot join the course, you are too late';
          ELSE
-            INSERT INTO Registered Values (NEW.student, New.code);
+            INSERT INTO Registered Values (NEW.student, NEW.course);
          END IF; 
         ELSE 
-            INSERT INTO Registered VALUES (NEW.student, NEW.code);
+            INSERT INTO Registered VALUES (NEW.student, NEW.course);
         END IF;  
     ELSE
     RAISE EXCEPTION 'You do not meet the Pre-requirements for the course, git g0d slacker(SIMON)';
-    END IF;       
+    END IF; 
+    RETURN NEW;      
     END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION unRegister () RETURNS trigger AS $$
+CREATE OR REPLACE FUNCTION unRegister() RETURNS trigger AS $$
 DECLARE
 BEGIN 
   IF EXISTS (SELECT student FROM Registrations WHERE Registrations.status = 'registered' AND NEW.student = student and new.course = course) 
@@ -40,13 +43,16 @@ BEGIN
   ELSEIF EXISTS (SELECT student FROM Registrations WHERE registrations.status = 'waiting' AND NEW.student = student and new.course = course)
   THEN  
    DELETE FROM WaitingList WHERE NEW.student = student;
-  END IF; 
+  END IF;
+  RETURN OLD; 
 END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE TRIGGER RegisteredTrigger BEFORE INSERT ON Registered
-    FOR EACH ROW EXECUTE FUNCTION CountRegistered();
+CREATE OR REPLACE TRIGGER RegisteredTrigger 
+INSTEAD OF INSERT ON Registrations
+FOR EACH ROW 
+EXECUTE FUNCTION CountRegistered();
 
 CREATE OR REPLACE TRIGGER unregistered
 INSTEAD OF DELETE ON Registrations
